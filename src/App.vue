c43760f1b26ee2cca3dd518051045b38c17d9560
GiuseppeDalessio7 <g.dalessio03@gmail.com>
2023-09-26 17:22:09 +0200

    Add header and select

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script >
+import Button from "../src/components/Button.vue"
+
+
+export default {
+  components: {
+    Button,
+
+
+  },
+  data() {
+    return {
+
+
+
+    }
+  }
+}
+
 </script>
 
 <template>
-  <h1 class="container">CIAO</h1>
+  <div class="d-flex align-items-center p-2">
+    <img src="../src/assets/img/logo.jpg" alt="">
+    <h1 class="m-2">Yu-Gi-Oh Api</h1>
+  </div>
+  <Button></Button>
 </template>
 
-<style scoped></style>
+<style scoped>
+img {
+  width: 70px;
+  height: 30px;
+}
+</style>
